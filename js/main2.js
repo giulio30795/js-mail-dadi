f569b10c35ccee75e490330853163d433ec8be69
giulio30795 <giulioincarbone95@gmail.com>
2021-10-26 15:45:35 +0200

    check3

--- js/main2.js
+++ js/main2.js
@@ -1,15 +1,12 @@
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
 // Definizione Variabili
 
 const winner = document.querySelector('.winner')
-
 const player = Math.floor(Math.random() * 6 + 1);
 const computer = Math.floor(Math.random() * 6 + 1);
 
-console.log(player);
-console.log(computer);
 
+// Determinare il vincitore
 if (player > computer){
     winner.innerHTML += 'player'
 } else if (player < computer){
